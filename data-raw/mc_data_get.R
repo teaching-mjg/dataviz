dad7256b365212c3b0dae610da26f388def2d3d5
mgreen1718 <mgreen1718@gmail.com>
2023-07-18 03:14:37 +0100

    add tania data

--- data-raw/mc_data_get.R
+++ data-raw/mc_data_get.R
@@ -1,23 +1,7 @@
-library("tidyverse")
+library("readr")
 
 # read the data in from the csv file
-mydata = read_csv("data_mc.csv")
+data_mc = read_csv("data-raw/mc_data.csv")
 
-# make the plot
-ggplot(data=mydata, 
-       aes(y     = mean_accuracy,
-           x     = Valence,
-           group = interaction(vidrep,Valence),
-           fill  = Valence))+
-  facet_grid(Age~vidrep)+
-  geom_violin(alpha=0.5)+
-  geom_boxplot(outlier.shape=NA, width = 0.2, fatten=0, show.legend = FALSE)+
-  stat_summary(fun="mean", geom="point", show.legend=FALSE)+
-  stat_summary(fun="mean", geom='line', group=1)+
-  labs(title = "Accuracy", subtitle = "Points and lines are mean values")+
-  ylab("Mean accuracy rates")
-
-# save the plot to file
-ggsave("plot_mc.png")
-
-# Excercise: Can you figure out how to plot z-scores instead of accuracy?
+### Save the cleaned data in the required R package location
+usethis::use_data(data_mc, overwrite = TRUE)
