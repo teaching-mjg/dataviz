fe56370f52c43c7536a614c083ed8cb8d202bff6
mgreen1718 <mgreen1718@gmail.com>
2023-07-20 11:29:54 +0100

    add dummy tutorial

--- inst/handy_snippets/common_build_commands.R
+++ inst/handy_snippets/common_build_commands.R
@@ -1,15 +1,20 @@
-devtools::build_readme()
-
 devtools::install()
+devtools::install(build_vignettes = TRUE)
 pkgdown::build_site_github_pages()
 
 
 library("dataviz")
 data(package="dataviz")
-
-
-devtools::install(build_vignettes = TRUE)
 vignette(package="dataviz")
 browseVignettes(package="dataviz")
 RShowDoc("basic-plotting", package="dataviz") # displays a “rendered” vignette (pdf or html).
 vignette(all = FALSE)                         # List vignettes from all *attached* packages
+
+
+devtools::build_readme()
+
+usethis::use_package("packagename")           # adds package to Depends
+
+usethis::use_tidy_description()
+
+usethis::use_tutorial("<name-of-learnr-file>", "<Title You'd Like the User to See>")
