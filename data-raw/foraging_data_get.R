691b93eb6937b6131deada1915df6db0b0444f12
mgreen1718 <mgreen1718@gmail.com>
2023-07-23 13:03:40 +0100

    foraging data edits

--- data-raw/foraging_data_get.R
+++ data-raw/foraging_data_get.R
@@ -6,9 +6,14 @@ data_foraging <- readr::read_csv("data-raw/foraging_data.csv")
 data_foraging <-
   data_foraging %>%
   mutate(
-    trees=as_factor(trees),
-    fruit=as_factor(fruits),
-    condition=as_factor(condition),
+    instructions = factor(trees,
+                          levels=c(labels=c("different forests", "same forest", "pp gets own forest")),
+                          labels=c("naive participants", "naive participants", "INFORMED participants")),
+    trees=factor(trees,
+                 levels=c("different forests", "same forest", "pp gets own forest"),
+                 labels=c("different forests", "same forest", "same forest")),
+    fruits=as_factor(fruits),
+    condition=paste(fruits, trees, sep=" : "),
     pid=as_factor(pid),
     phase=as_factor(phase),
     complete=as_factor(complete),
@@ -20,5 +25,6 @@ data_foraging <-
     use_master_forest=as_factor(use_master_forest),
     redistribute_fruit=as_factor(redistribute_fruit),
     high_fruit=as_factor(high_fruit)
-  )
+  ) %>%
+  mutate(condition=as_factor(condition))
 usethis::use_data(data_foraging, overwrite = TRUE)
--- inst/handy_snippets/common_build_commands.R
+++ inst/handy_snippets/common_build_commands.R
@@ -1,6 +1,10 @@
-devtools::install()
-devtools::install(build_vignettes = TRUE)
+devtools::document()                          # roxygen will flesh out the .Rd file using data from the DESCRIPTION.
+devtools::build_readme()
 pkgdown::build_site_github_pages()
+devtools::install()
+
+
+devtools::install(build_vignettes = TRUE)
 
 
 library("dataviz")
@@ -11,7 +15,7 @@ RShowDoc("basic-plotting", package="dataviz") # displays a “rendered” vignet
 vignette(all = FALSE)                         # List vignettes from all *attached* packages
 
 
-devtools::build_readme()
+
 
 usethis::use_package("packagename")           # adds package to Depends
 
@@ -19,7 +23,6 @@ usethis::use_tidy_description()
 
 usethis::use_tutorial("<name-of-learnr-file>", "<Title You'd Like the User to See>")
 
-devtools::document()                          # roxygen will flesh out the .Rd file using data from the DESCRIPTION.
 
 devtools::load_all()
 fs::path_package("inst", "slides", "slides.qmd", package = "dataviz")
