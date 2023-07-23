48e3b9661323c233d42d46b9c2db0493612289ab
mgreen1718 <mgreen1718@gmail.com>
2023-07-23 12:19:37 +0100

    foraging data factors

--- data-raw/foraging_data_get.R
+++ data-raw/foraging_data_get.R
@@ -1,2 +1,24 @@
+library(readr)
+library(magrittr)
+library(dplyr)
+library(forcats)
 data_foraging <- readr::read_csv("data-raw/foraging_data.csv")
+data_foraging <-
+  data_foraging %>%
+  mutate(
+    trees=as_factor(trees),
+    fruit=as_factor(fruits),
+    condition=as_factor(condition),
+    pid=as_factor(pid),
+    phase=as_factor(phase),
+    complete=as_factor(complete),
+    sex=as_factor(sex),
+    ethnic = as_factor(ethnic),
+    nation=as_factor(nation),
+    OS=as.factor(OS),
+    textbox.text=as_factor(textbox.text),
+    use_master_forest=as_factor(use_master_forest),
+    redistribute_fruit=as_factor(redistribute_fruit),
+    high_fruit=as_factor(high_fruit)
+  )
 usethis::use_data(data_foraging, overwrite = TRUE)
